f25a86aa82aa22c5c85ee557486de5ac06465c20
Anna Moelling <anna.moelling@googlemail.com>
2021-09-25 10:59:53 +0200

    create dates_apart helper function

--- portagon.rb
+++ portagon.rb
@@ -1,6 +1,12 @@
+require 'date'
+
 INTEREST_RATE = 0.05
 REPAYMENT_PERCENTAGE = 0.25
 INITIAL_AMOUNT = 1000
 INITIAL_DATE = Date.new(2020, 2, 8)
 FIRST_REPAYMENT_DATE = Date.new(2020, 6, 30)
 REPAYMENT_INTERVAL_IN_YEARS = 1
+
+def days_apart(start_date, future_date)
+  (future_date - start_date).to_i
+end
