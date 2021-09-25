2a276559a40a352dceae13fe0f8c99b222125836
Anna Moelling <anna.moelling@googlemail.com>
2021-09-25 11:08:30 +0200

    implement interest calculation

--- portagon.rb
+++ portagon.rb
@@ -10,3 +10,25 @@ REPAYMENT_INTERVAL_IN_YEARS = 1
 def days_apart(start_date, future_date)
   (future_date - start_date).to_i
 end
+
+def interest_payment(amount, interest_rate, days_count, repayment_date) 
+  year_in_days = days_apart(repayment_date.prev_year, repayment_date)
+  puts 'year in days'
+  puts year_in_days
+  
+  return amount * interest_rate *  days_count / year_in_days
+end
+
+days_count = days_apart(
+  INITIAL_DATE, 
+  FIRST_REPAYMENT_DATE
+)
+puts 'days_count'
+puts days_count
+
+puts interest_payment(
+  INITIAL_AMOUNT, 
+  INTEREST_RATE,
+  days_count, 
+  FIRST_REPAYMENT_DATE
+) 
