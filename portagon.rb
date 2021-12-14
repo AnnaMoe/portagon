58fe229a3dea8422805c34585d953df037b5a19c
Anna Moelling <anna.moelling@googlemail.com>
2021-12-14 22:58:47 +0100

    move calculate interest into a callable

--- portagon.rb
+++ portagon.rb
@@ -14,28 +14,36 @@ YEARS_TO_CALCULATE = 10
 
 # main function
 class Solution
+  def initialize
+    @previous_date = INITIAL_DATE
+    @current_date = FIRST_REPAYMENT_DATE
+    @current_amount = INITIAL_AMOUNT
+  end
+
   def main  
-    previous_date = INITIAL_DATE
-    current_date = FIRST_REPAYMENT_DATE
-    current_amount = INITIAL_AMOUNT
     (0..YEARS_TO_CALCULATE).each do 
-      interest = InterestCalculator.compute_interest(
-        current_amount, 
-        INTEREST_RATE,
-        previous_date, 
-        current_date
-      )
+      interest = calculate_interest()
 
-      repayment = RepaymentCalculator.compute_repayment(current_amount, REPAYMENT_PERCENTAGE)
+      repayment = RepaymentCalculator.compute_repayment(@current_amount, REPAYMENT_PERCENTAGE)
 
-      new_amount = YearlyAmountCalculator.compute_yearly_amount(current_amount, repayment, interest)
+      new_amount = YearlyAmountCalculator.compute_yearly_amount(@current_amount, repayment, interest)
 
-      puts "#{current_date} Previous Amount: #{current_amount}, Interest: #{interest}, Repayment: #{repayment}, New Amount: #{new_amount}"
+      puts "#{@current_date} Previous Amount: #{@current_amount}, Interest: #{interest}, Repayment: #{repayment}, New Amount: #{new_amount}"
       # update values
-      current_amount = new_amount
-      previous_date = current_date
-      current_date = current_date.next_year
+      @current_amount = new_amount
+      @previous_date = @current_date
+      @current_date = @current_date.next_year
     end
-    current_amount
+    @current_amount
+  end
+  
+  private
+  def calculate_interest
+    InterestCalculator.compute_interest(
+        @current_amount, 
+        INTEREST_RATE,
+        @previous_date, 
+        @current_date
+      )
   end
 end
